f3084c2354d6c0c5db634e0e8946dd392792f361
Adam Gutonski <adamgutonski@protonmail.com>
2020-01-14 22:15:47 -0500

    script that seems to be working, copies .gdb files to .shps in a folder, need to work input

--- qgis_test.py
+++ qgis_test.py
@@ -2,19 +2,54 @@ import os
 from qgis.core import *
 from osgeo import ogr
 
+
+def copy_shps():
+	QgsApplication.setPrefixPath("/usr/bin/qgis", True)
+
+	# Create a reference to the QgsApplication.  Setting the
+	# second argument to False disables the GUI.
+	qgs = QgsApplication([], False)
+
+	qgs.initQgis()
+
+
+	driver = ogr.GetDriverByName("OpenFileGDB")
+
+	ds = driver.Open("/home/bedevere/Qgis_Python/RH_SampleData.gdb")#, 0)
+
+	layer_idxs = ds.GetLayerCount()
+
+	#layer = ds.GetLayer()
+
+
+	for lyr in range(layer_idxs):
+		uri = "/home/bedevere/Qgis_Python/RH_SampleData.gdb|layerid=%s" % lyr
+		ogr_layer = ds.GetLayer(lyr)
+		try:
+			layer = QgsVectorLayer(uri, "layer_name_you_like", "ogr")
+		except:
+			print "Layer is invalid!"
+		layer_name = ogr_layer.GetName()
+		writer = QgsVectorFileWriter.writeAsVectorFormat(layer,r"/home/bedevere/Qgis_Python/dump/%s_%s.shp" % (layer_name, lyr),"utf-8",None,"ESRI Shapefile")
+		#layer = ds.GetLayer(lyr)
+		#layer_name = layer.GetName()
+		#print layer_name
+		del writer
+		del layer
+		del ogr_layer
+
+	# it seems like deleting the driver released the segmentation fault error this time...i bet b/c it held on to the file .gdb 
+	# through ogr while qgis also used the same files to createa  layer...
+	del driver
+	del ds
+
+	qgs.exitQgis()
+
+
+copy_shps()
+
+
 """
-driver = ogr.GetDriverByName("OpenFileGDB")
-
-ds = driver.Open("/home/bedevere/Qgis_Python/RH_SampleData.gdb", 0)
-
-print ds.GetLayerCount()
-
-for i in range(ds.GetLayerCount()):
-	lyr = ds.GetLayerByIndex(i)
-	ds.CopyLayer(lyr, str(i))
-	del lyr
-"""
-
 #### This little chunk copies a file geodatabase layer out to shapefile format... #######################
 
 QgsApplication.setPrefixPath("/usr/bin/qgis", True)
@@ -49,6 +84,7 @@ del quit
 ##################################################################################################################
 
 """
+"""
 path_to_ports_layer = os.path.join("/home", "bedevere", "Qgis_Python", "Sample_Data", "exercise_data", "projected_data", "farms_33S.shp")
 
 vlayer = QgsVectorLayer(path_to_ports_layer, "Ports layer", "ogr")
@@ -72,7 +108,7 @@ del vlayer
 # Finally, exitQgis() is called to remove the
 # provider and layer registries from memory
 
-qgs.exitQgis()
+#qgs.exitQgis()
 
 
 
